68b7f56f242b7bad341aa8ffa06982ca48866991
Naga Lakshmi <nagalakshmikandregula@gmail.com>
2024-05-15 22:55:07 +0530

    EDAand Problems statement

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from setuptools import find_packages,setup
 from typing import List
-
+ 
+HYPEN_E_DOT = '-e .'
 def get_requirements(file_path:str)->List[str]:
     '''
     This function will return the list of requirements
@@ -11,10 +12,14 @@ def get_requirements(file_path:str)->List[str]:
         requirements=file_obj.readlines()
         requirements = [req.replace ("\n","") for req in requirements]
 
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+
+    return requirements
 
 setup(
 name ='mlproject',
-version = '0.0.1',
+version = '0.0.2',
 author = 'Lakshmi',
 author_email = 'nagalakshmikandregula@gmail.com',
 packages = find_packages(),
